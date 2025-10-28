502ed349b95b2a0fc848fb1f3671408da72cbf16
CODEsane04 <debjitcodes18@gmail.com>
2025-10-28 19:14:36 +0530

    replace the short url (which was earlier pointing to frontned/UID) to https://url-backend-mrpf.onrender.com/URL

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -20,7 +20,7 @@ async function handleGenerateNewShortUrl(req, res) {
 
     try {
 
-        const short_url = `http://urlshortener-blush-nu.vercel.app/${UID}`;
+        const short_url = `https://url-backend-mrpf.onrender.com/${UID}`;
 
         await URL.create({
             shortId: UID,
